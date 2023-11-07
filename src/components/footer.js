e72791f5a0cc1ef30466248b7f271e73829ebb7e
sharawi <adnanmish30@gmail.com>
2023-11-07 11:34:34 +0000

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,54 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
+import { faSquareFacebook, faSquareInstagram, faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
-    return ( 
-        <div className="footer">
-            <div className="text about">
-                <h5>UNIQUEST</h5>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, quam? Possimus natus ad sequi consequatur qui architecto nulla quae iste atque tempora distinctio illo ex veniam earum, neque fuga officia molestias eum ipsum corporis perferendis!
+    return (
+        <footer>
+            <div className="footer">
+                <div className="text uniquest">
+                    <div>
+                        <h6>UNIQUEST</h6>
+                        <p>Get information about every tertiary program, school, available scholarships,  and career opportunities in one place.</p>
+                    </div>
+                    <div className="social">
+                        <a href=""><FontAwesomeIcon icon={faSquareTwitter} className="socialIcon" /></a>
+
+                        <a href=""><FontAwesomeIcon icon={faSquareInstagram} className="socialIcon" /></a>
+
+                        <a href=""><FontAwesomeIcon icon={faSquareFacebook} className="socialIcon" /></a>
+                    </div>
+                </div>
+
+                <div className="aboutContact">
+                    <div className="text explore">
+                        <h6>EXPLORE</h6>
+                        <ul>
+                            <li><FontAwesomeIcon icon={faGreaterThan} className="arrow" /><Link to='/'>Careers</Link></li>
+                            <li><FontAwesomeIcon icon={faGreaterThan} className="arrow" /><Link to='/'>Results check</Link></li>
+                            <li><FontAwesomeIcon icon={faGreaterThan} className="arrow" /><Link to='/'>Support</Link></li>
+                        </ul>
+                    </div>
+
+                    <div className="text about">
+                        <h6>ABOUT</h6>
+                        <ul>
+                            <li><FontAwesomeIcon icon={faGreaterThan} className="arrow" /><Link to='/'>Uniquest</Link></li>
+                            <li><FontAwesomeIcon icon={faGreaterThan} className="arrow" /><Link to='/'>Team</Link></li>
+                            <li><FontAwesomeIcon icon={faGreaterThan} className="arrow" /><Link to='/'>Terms of services</Link></li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
-            <div className="text contact">
-                <h5>Contact</h5>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate nisi reprehenderit accusamus optio asperiores illum labore incidunt officiis nobis, rem laboriosam ipsa nulla sunt, voluptatibus voluptatem odit rerum cum ipsam.
+
+            <div className="cprt">
+                <div></div>
+                <p>Copyright © 2023 Uniquest. All Rights Reserved</p>
             </div>
-        </div>
-     );
+        </footer>
+    );
 }
- 
+
 export default Footer;
